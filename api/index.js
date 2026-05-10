005bb09af488e219e8ee4c5fc0bcfcd8ae20326c
vumzizs <daniels.reders@gmail.com>
2026-05-10 16:02:51 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,21 +1,5 @@
-const app = require('./server')
-const logger = require('./logger')
+const app = require('./server');
+const logger = require('./logger');
 
-const port = process.env.PORT || 3000
-
-app.listen(port, () => {
-  const banner = `
-██╗███╗   ███╗ █████╗  ██████╗ ███████╗    ██╗      ██████╗  ██████╗  ██████╗ ███████╗██████╗ 
-██║████╗ ████║██╔══██╗██╔════╝ ██╔════╝    ██║     ██╔═══██╗██╔════╝ ██╔════╝ ██╔════╝██╔══██╗
-██║██╔████╔██║███████║██║  ███╗█████╗      ██║     ██║   ██║██║  ███╗██║  ███╗█████╗  ██████╔╝
-██║██║╚██╔╝██║██╔══██║██║   ██║██╔══╝      ██║     ██║   ██║██║   ██║██║   ██║██╔══╝  ██╔══██╗
-██║██║ ╚═╝ ██║██║  ██║╚██████╔╝███████╗    ███████╗╚██████╔╝╚██████╔╝╚██████╔╝███████╗██║  ██║
-╚═╝╚═╝     ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝    ╚══════╝ ╚═════╝  ╚═════╝  ╚═════╝ ╚══════╝╚═╝  ╚═╝`
-
-  process.platform === 'linux' || process.platform === 'darwin'
-    ? process.stdout.write(`\x1Bc\n\n${banner}\n\n`)
-    : process.stdout.write(`\x1B[2J\x1B[3J\x1B[H\n\n${banner}\n\n`)
-
-  console.log(`Server started on port ${port}`)
-  logger.info('Image logger started')
-})
+// This replaces all that "app.listen" code you had before
+module.exports = app;
